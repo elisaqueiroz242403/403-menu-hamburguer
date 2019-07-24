92e782ab8283863b4a134d67b2d4122588f3ee73
karen queiroz <kqueiroz812@gmail.com>
2019-07-24 16:25:53 -0300

    adicionar menu hamburguer

---
+++ script.js
@@ -0,0 +1,12 @@
+let hamburguer = document.querySelector("#hamburguer")
+let menu = document.querySelector("section")
+let tracos = document.querySelectorAll("span")
+
+
+function controlarMenu(){
+    menu.classList.toggle("aparecerMenu");
+    tracos[1].classList.toggle
+}
+
+hamburguer.onclick = controlarMenu;
+
